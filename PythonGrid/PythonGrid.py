dd47b739d0a4c0321e32752835f4909f9ff9f262
Joel Howe <31639333+JoelHowe@users.noreply.github.com>
2019-02-26 01:57:26 -0400

    Initial commit, generating a simple grid

--- PythonGrid/PythonGrid.py
+++ PythonGrid/PythonGrid.py
@@ -1,1 +1,30 @@
 
+from graphics import *
+
+def grid():
+    win = GraphWin("My Grid", 1001, 601)
+    win.setBackground("white")
+    win.getMouse()
+    columns = 0
+    x1 = 1
+    x2 = 100
+    y1 = 100
+    y2 = 1    
+    while columns < 10:
+        rows = 0
+        while rows < 6:
+            square = Rectangle(Point(x1,y1), Point(x2,y2))
+            square.draw(win)
+            y1 += 100
+            y2 += 100
+            rows += 1
+        columns += 1
+        x1 += 100
+        x2 += 100
+        y1 = 100
+        y2 = 1
+   
+    win.getMouse() # pause for click in window
+    win.close()
+
+grid()
